c2185206c21d3f63ba79880cb6022e7b23ca566a
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-29 23:44:34 +0500

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import { ComboboxDemo } from "./combobox";
 
 export default function Home() {
   return (
+    <>
    <h1>Shadcn-ui</h1>
+   <ComboboxDemo/>
+   </>
   );
 }
