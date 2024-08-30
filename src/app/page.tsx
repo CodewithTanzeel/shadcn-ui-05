60c70ac2e31c4c6220834f19be75095fa743430b
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-30 12:44:32 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,17 @@ import { ComboboxDemo } from "./combobox";
 
 export default function Home() {
   return (
-    <div className="p-5 justify-center items-center text-center ">
+    <main className="">
+    <div className="
+    justify-center items-center text-center pt-10 p-25 ">
 
-   <h1 className="">Shadcn-ui</h1>
-   <div>
+   <h1 className="text-4xl ">Shadcn-ui</h1>
+   <div className="items-center ">
    <ComboboxDemo/>
    </div>
+  
     </div>
+    </main>
 
   );
 }
